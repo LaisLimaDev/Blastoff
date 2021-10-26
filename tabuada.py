8178c1fb2e16b69dd53462dad31627d1d8c1c01c
Lais Lima <laissp25@gmail.com>
2021-10-26 13:26:58 -0300

    exercícios 8 e 9

---
+++ tabuada.py
@@ -0,0 +1,7 @@
+""" - Faça um algoritmo que receba um número e retorne a tabuada desse número. """
+
+
+num=int(input("Digite um número: "))
+for c in range (10):
+    print(num,'x', c,'=',(num* c))
+
